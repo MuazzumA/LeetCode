32b416059ec1670611ac62848f8c77ce0a2ebd09
Muazzum Akhtar <85028517+MuazzumA@users.noreply.github.com>
2023-08-18 18:15:35 -0400

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,3 +1,7 @@
+# Given an array of integers and an integer target, return indices of the two numbers such that they add up to target.
+# 'nums': list of integers
+# 'target': integer representing the sum 
+# '-> List[int]': function will return a list of integers (the indices of the numbers that satisfy condition.
 class Solution:
   def twoSum(self, nums: List[int], target: int) -> List[int]:
     prevMap = {} # val : index
